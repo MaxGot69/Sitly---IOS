e307f2a2b88e7a5fdb79f46e35f9bd98aadcd001
Maxim Gotovchenko <gotovchenko04@bk.ru>
2025-06-24 15:21:17 +0300

    Initial Commit

---
+++ Sitly/SitlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SitlyApp.swift
+//  Sitly
+//
+//  Created by Maxim Gotovchenko on 24.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SitlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SitlyTests/SitlyTests.swift
@@ -0,0 +1,16 @@
+//
+//  SitlyTests.swift
+//  SitlyTests
+//
+//  Created by Maxim Gotovchenko on 24.06.2025.
+//
+
+import Testing
+
+struct SitlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
